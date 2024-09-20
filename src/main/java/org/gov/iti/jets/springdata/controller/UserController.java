d7b19d49a6c4d06938eb37185d23fc792396fed2
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2024-09-20 16:26:29 +0300

    remove all logic

--- src/main/java/org/gov/iti/jets/springdata/controller/UserController.java
+++ src/main/java/org/gov/iti/jets/springdata/controller/UserController.java
@@ -1,10 +1,10 @@
 package org.gov.iti.jets.springdata.controller;
 
-import org.gov.iti.jets.springdata.models.Student;
-import org.gov.iti.jets.springdata.repositories.StudentRepository;
-import org.gov.iti.jets.springdata.service.StudentService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+//import org.gov.iti.jets.springdata.models.Student;
+//import org.gov.iti.jets.springdata.repositories.StudentRepository;
+//import org.gov.iti.jets.springdata.service.StudentService;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,28 +12,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/students")
 public class UserController {
-
-    private final StudentRepository studentRepository;
-    private final StudentService studentService;
-
-    public UserController(StudentRepository studentRepository, StudentService studentService) {
-        this.studentRepository = studentRepository;
-        this.studentService = studentService;
-    }
-
-    @PostMapping
-    public ResponseEntity<Student> addNewStudent(@RequestBody Student student) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(studentRepository.save(student));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Student>> getAllStudent() {
-        return ResponseEntity.status(HttpStatus.OK).body(studentService.getAllStudent());
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable Long id) {
-        studentService.deleteStudentById(id);
-        return ResponseEntity.ok().build();
-    }
+//
+//    private final StudentRepository studentRepository;
+//    private final StudentService studentService;
+//
+//    public UserController(StudentRepository studentRepository, StudentService studentService) {
+//        this.studentRepository = studentRepository;
+//        this.studentService = studentService;
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<Student> addNewStudent(@RequestBody Student student) {
+//        return ResponseEntity.status(HttpStatus.CREATED).body(studentRepository.save(student));
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<List<Student>> getAllStudent() {
+//        return ResponseEntity.status(HttpStatus.OK).body(studentService.getAllStudent());
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable Long id) {
+////        studentService.deleteStudentById(id);
+//        return ResponseEntity.ok().build();
+//    }
 }
