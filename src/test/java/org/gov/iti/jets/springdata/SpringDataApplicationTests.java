62a0f98f101c810de377d35cc1f36d173a16278a
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2024-09-26 23:10:27 +0300

    add nw comment

--- src/test/java/org/gov/iti/jets/springdata/SpringDataApplicationTests.java
+++ src/test/java/org/gov/iti/jets/springdata/SpringDataApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataApplicationTests {
-    // comment
+    // comment 2
     @Test
     void contextLoads() {
         // This is new comment
