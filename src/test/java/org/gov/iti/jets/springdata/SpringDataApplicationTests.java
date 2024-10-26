f1531fe87a50f80cd0bc86f92bf196d5e78e826e
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2024-10-27 01:27:22 +0300

    add mongo database

--- src/test/java/org/gov/iti/jets/springdata/SpringDataApplicationTests.java
+++ src/test/java/org/gov/iti/jets/springdata/SpringDataApplicationTests.java
@@ -1,14 +1,20 @@
 package org.gov.iti.jets.springdata;
 
+import org.gov.iti.jets.springdata.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataApplicationTests {
+
+    @Autowired
+    private BookRepository bookRepository;
     // comment 2
     @Test
     void contextLoads() {
         // This is new comment
+        bookRepository.findAll().forEach(System.out::println);
     }
 
 }
