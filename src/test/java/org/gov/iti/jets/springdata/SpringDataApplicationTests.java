2db5bc95225df5f6e6c949af0e311ae1fa010547
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2025-01-10 15:11:14 +0300

    add if conditions for testing

--- src/test/java/org/gov/iti/jets/springdata/SpringDataApplicationTests.java
+++ src/test/java/org/gov/iti/jets/springdata/SpringDataApplicationTests.java
@@ -1,5 +1,7 @@
 package org.gov.iti.jets.springdata;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.gov.iti.jets.springdata.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +12,12 @@ class SpringDataApplicationTests {
 
     @Autowired
     private BookRepository bookRepository;
+
     // comment 2
     @Test
     void contextLoads() {
         // This is new comment
+        assertEquals(10, 6);
         bookRepository.findAll().forEach(System.out::println);
     }
 
