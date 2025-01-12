c115bf6ff0f5490fa46157e398c9ae12304d00d5
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2025-01-12 23:45:08 +0300

    remove all services

--- src/main/java/org/gov/iti/jets/springdata/SpringDataApplication.java
+++ src/main/java/org/gov/iti/jets/springdata/SpringDataApplication.java
@@ -1,34 +1,14 @@
 package org.gov.iti.jets.springdata;
 
-import org.gov.iti.jets.springdata.models.Book;
-import org.gov.iti.jets.springdata.repositories.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 //
 
 @SpringBootApplication
-// public class SpringDataApplication {
-public class SpringDataApplication implements CommandLineRunner {
-
-    @Autowired
-    private BookRepository bookRepository;
+public class SpringDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        bookRepository.saveAll(List.of(
-                Book.builder().bookName("Java EE").author("Ebrahim").build(),
-                Book.builder().bookName("Hibernate").author("JavaBrains").build(),
-                Book.builder().bookName("Maven").author("Hamza").build(),
-                Book.builder().bookName("JPA").author("Hamada").build()));
-        bookRepository.findAll().forEach(System.out::println);
-
-    }
 }
