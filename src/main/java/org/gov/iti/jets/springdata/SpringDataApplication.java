4633019d0afb9ec45bbff7e65094af3142cd1b41
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2025-01-17 15:32:03 +0300

    added new workflow with envs and secrets

--- src/main/java/org/gov/iti/jets/springdata/SpringDataApplication.java
+++ src/main/java/org/gov/iti/jets/springdata/SpringDataApplication.java
@@ -1,14 +1,23 @@
 package org.gov.iti.jets.springdata;
 
+import org.gov.iti.jets.springdata.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
 
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+
+    @Autowired
+    private BookRepository bookRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        bookRepository.findAll().forEach(System.out::println);
+    }
 }
---
+++ src/main/java/org/gov/iti/jets/springdata/models/Book.java
@@ -0,0 +1,25 @@
+package org.gov.iti.jets.springdata.models;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "books")
+@ToString
+public class Book {
+
+    @Id
+    private String id;
+
+    private String bookName;
+
+    private String author;
+
+}
