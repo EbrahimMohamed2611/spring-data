8c2b0444fd6b19996a806811cdb2151b8f825514
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2025-01-17 15:46:45 +0300

    added new workflow with envs and secrets

--- src/main/java/org/gov/iti/jets/springdata/SpringDataApplication.java
+++ src/main/java/org/gov/iti/jets/springdata/SpringDataApplication.java
@@ -1,5 +1,6 @@
 package org.gov.iti.jets.springdata;
 
+import lombok.RequiredArgsConstructor;
 import org.gov.iti.jets.springdata.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -7,10 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class SpringDataApplication implements CommandLineRunner {
 
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataApplication.class, args);
--- src/main/java/org/gov/iti/jets/springdata/controller/BookController.java
+++ src/main/java/org/gov/iti/jets/springdata/controller/BookController.java
@@ -1,30 +1,30 @@
 package org.gov.iti.jets.springdata.controller;
 
-//import org.gov.iti.jets.springdata.models.Student;
-//import org.gov.iti.jets.springdata.repositories.StudentRepository;
-//import org.gov.iti.jets.springdata.service.StudentService;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
+import org.gov.iti.jets.springdata.models.Book;
+import org.gov.iti.jets.springdata.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
-//    private final BookService bookService;
-//
-//    public BookController(BookService bookService) {
-//        this.bookService = bookService;
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<Book> addNewStudent(@RequestBody Book book) {
-//        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.saveBook(book));
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<List<Book>> getAllStudent() {
-//        return ResponseEntity.status(HttpStatus.OK).body(bookService.getAllBooks());
-//    }
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> addNewStudent(@RequestBody Book book) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.saveBook(book));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllStudent() {
+        return ResponseEntity.status(HttpStatus.OK).body(bookService.getAllBooks());
+    }
 }
