800060019a9533697e40fa9b4e86cc8cbc7f81ea
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2025-02-12 01:57:53 +0200

    add some data

--- src/main/java/org/gov/iti/jets/springdata/SpringDataApplication.java
+++ src/main/java/org/gov/iti/jets/springdata/SpringDataApplication.java
@@ -1,8 +1,9 @@
 package org.gov.iti.jets.springdata;
 
 import lombok.RequiredArgsConstructor;
+import org.gov.iti.jets.springdata.models.Book;
 import org.gov.iti.jets.springdata.repositories.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +20,11 @@ public class SpringDataApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.saveAll(List.of(
+                Book.builder().bookName("Java EE").author("Ebrahim").build(),
+                Book.builder().bookName("Hibernate").author("JavaBrains").build(),
+                Book.builder().bookName("Maven").author("Hamza").build(),
+                Book.builder().bookName("JPA").author("Hamada").build()));
         bookRepository.findAll().forEach(System.out::println);
     }
 }
