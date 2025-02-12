25a02d0206a8dce4ad260ead4b8d3a0d80e50ce8
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2025-02-12 02:00:26 +0200

    add some data

--- src/test/java/org/gov/iti/jets/springdata/SpringDataApplicationTests.java
+++ src/test/java/org/gov/iti/jets/springdata/SpringDataApplicationTests.java
@@ -1,9 +1,12 @@
 package org.gov.iti.jets.springdata;
 
+import org.gov.iti.jets.springdata.models.Book;
 import org.gov.iti.jets.springdata.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 
 @SpringBootTest
@@ -16,7 +19,11 @@ class SpringDataApplicationTests {
     // comment 2
     @Test
     void contextLoads() {
-        // This is new comment
+        bookRepository.saveAll(List.of(
+                Book.builder().bookName("Java EE").author("Ebrahim").build(),
+                Book.builder().bookName("Hibernate").author("JavaBrains").build(),
+                Book.builder().bookName("Maven").author("Hamza").build(),
+                Book.builder().bookName("JPA").author("Hamada").build()));
         bookRepository.findAll().forEach(System.out::println);
     }
 
